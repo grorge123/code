7ee61541d3ec8bc7344d099f96eb0e0c29a44de0
grorge <g97116@yahoo.com>
2017-09-02 19:06:33 +0800

    20170902

--- c++/新文件1.cpp
+++ c++/新文件1.cpp
@@ -1,21 +1,11 @@
 #include <stdio.h>
-int main()
-{
- char a[5], i;
- for(i=0; i<5; i++)
-   scanf("%s",&a[i]);
- for(i=4; i>=0; i--)
-   printf("%s",a[i]);
-}
-/*#include<stdio.h>
 int main(){
-char a[4];
-for(int i =0;i<5;i++){
-	scanf("%s",&a[i]);
-	
+char a[10];
+for(int i=0;i<5;i++){
+	scanf("%c",&a[i]);
 	}
 for(int i=0;i<5;i++){
-	printf("%s",a[i]);
+	printf("%c\n",a[i]);
 	
 	}
-}*/
+}
