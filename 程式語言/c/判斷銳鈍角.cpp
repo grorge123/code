7edcccf3ebb03b111957502057062532d478b7eb
grorge <g97116@yahoo.com>
2017-09-15 10:02:46 +0800

    20170915

---
+++ 程式語言/c/判斷銳鈍角.cpp
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main(){
+	int a,b,c;
+	printf("請輸入3個度數");
+	scanf("%d%d%d",&a,&b,&c);
+	if(a+b+c!=180){
+		goto end;
+	}
+	if(a==90||b==90||c==90){
+		printf("直角三角形");
+	}else{	
+	if(a>90||b>90||c>90){
+		printf("鈍角三角形");
+	}else{
+		printf("銳角三角形");
+	}
+	}
+if(5<3){
+end:;
+printf("沒這三角形");
+}
+
+}
