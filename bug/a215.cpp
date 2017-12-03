31c4ebc58a8e04c8d40dc7a9fdfcfab2fd7a64c5
grorge <g97116@yahoo.com>
2017-12-03 16:02:34 +0800

    20171203

--- bug/a215.cpp
+++ bug/a215.cpp
@@ -1,14 +1,14 @@
 #include <stdio.h>
  int main(){
-	int a,b;
-	while(scanf("%d %d",&a,&b)==2){
+	long a,b;
+	while(scanf("%lu %lu",&a,&b)==2){
 		int all=0,q=0;
 		for(int i=a;all<b;i++){
 			all+=i;
 			q++;
 			
 		}
-		printf("%d\n",q);
+		printf("%lu\n",q);
 	}
 }
 
