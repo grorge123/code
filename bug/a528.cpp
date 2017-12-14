8fd898cb1881f148792581953265bc1569ce43e2
grorge123 <g97116@yahoo.com>
2017-12-14 21:48:15 +0800

    Update and rename bug/a528.cpp to  bug/a528.cpp

--- bug/a528.cpp
+++ bug/a528.cpp
@@ -1,10 +1,10 @@
-#include<stdio.h>
+#include<stdio.h>#include<stdio.h>
 #include<string.h>
 #include<iostream>
 using namespace std;
 
 int main() {
-    int a;
+    int a,cont=0;
     while ( cin >> a){
         char d[1005][105]={};
         int s[1005][105]={};
@@ -19,12 +19,12 @@ int main() {
         for(int i=0;i<a;i++){
             for(int q=0;q<a-1;q++){
                 int a=strlen(d[q]),b=strlen(d[q+1]);
-                if(d[q+1][0]!='-'){
+                if(d[q+1][0]!='-'&&d[q][0]!='-'){
                     if(a>b){
                         swap(s[q],s[q+1]);
                         swap(d[q],d[q+1]);
                     }
-                    if(a>=b){
+                    if(a==b){
                         for(int sd=a;sd>-1;sd--){
                             if(s[q][sd]>s[q+1][sd]){
                         swap(s[q],s[q+1]);
@@ -32,12 +32,15 @@ int main() {
                             }
                         }
                     }
-                }else{
+                }else if(d[q+1][0]=='-'&&d[q][0]!='-'){
+                        swap(s[q],s[q+1]);
+                        swap(d[q],d[q+1]);
+                }else if(d[q+1][0]=='-'&&d[q][0]=='-'){
                     if(a<b){
                         swap(s[q],s[q+1]);
                         swap(d[q],d[q+1]);
                     }
-                    if(a>=b){
+                    if(a==b){
                         for(int sd=a;sd>-1;sd--){
                             if(s[q][sd]<s[q+1][sd]){
                         swap(s[q],s[q+1]);
