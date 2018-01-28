af693161ddfcae460877ce95fe62691d302fa277
grorge <g97116@yahoo.com>
2018-01-29 07:41:55 +0800

    20180129

--- bug/2.cpp
+++ bug/2.cpp
@@ -1,42 +1,35 @@
-#include <stdio.h>
-#include<iostream>
+#include<bits/stdc++.h>
 using namespace std;
-#include <string.h>
-int MazeSize = 7, CommandLen = 5+1, NumCommands = 10;
-void RobotMove(int *x,int *y,const char maze[MazeSize][MazeSize],const char commands[NumCommands][CommandLen]){
-    int t=0;
-    while(commands[t]!="END"){
-        if(commands[t]=="UP"&&maze[*x][*y+1]=='.')*y+=1;
-        if(commands[t]=="DOWM"&&maze[*x][*y-1]=='.')*y-=1;
-        if(commands[t]=="RIGHT"&&maze[*x+1][*y]=='.')*x+=1;
-        if(commands[t]=="LEFT"&&maze[*x-1][*y]=='.')*x-=1;
-    t++;
+unsigned long long int a[110]={};
+int v(int i){
+    if(i==1)return 1;
+    if(i==2)return 2;
+    if(a[i]!=0)return a[i];
+    a[i]=v(i-1)+v(i-2);
+    return a[i];
+}
+int main(){
+    int i;
+    while(cin >> i){
+        a[1]=1;
+        a[2]=2;
+        cout << v(i)<<endl;
     }
-
 }
 
-// ------------------------------------------
+#include <iostream>
+using namespace std;
 int main()
 {
-    char maze[MazeSize][MazeSize]= {};
-    for (int i=MazeSize-1; i>=0; i-=1)
-    {
-        for (int j=0; j<MazeSize; j+=1)
-        {
-            scanf(" %c", &maze[i][j]);
-        }
-    }
-    char commands[NumCommands][CommandLen]= {};
-    int t=0;
-    scanf("%s",commands[t]);
-    while(commands[t]!="END"){
-        t++;
-        scanf("%s",commands[t]);
-    }
+int i,n;
 
-    int x=0, y=0;
-    scanf("%d%d", &y, &x);
-    RobotMove(&x, &y, maze, commands);
-    printf("Now the robot is at (%d, %d).\n", y, x);
-    return 0;
+long long arr[101];
+    arr[1] = 1;
+    arr[2] = 2;
+    for(i=3;i<100;i++)
+        arr[i] = arr[i-1] + arr[i-2];
+    while(cin >> i)
+        cout << arr[i]<<endl;
+return 0;
+
 }
