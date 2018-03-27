65eb1275d2bd14a0813a086e3d0972aa42f42c6c
grorge123 <g97116@yahoo.com>
2018-03-27 22:21:17 +0800

    Update 二分收巡.cpp

--- 程式競賽/演算法/二分收巡.cpp
+++ 程式競賽/演算法/二分收巡.cpp
@@ -9,17 +9,25 @@ int main(){
     sort(s,s+n);
     int k;
     while(cin >> k){
-        int l=0,r=n-1;
-        while(l!=r){
+        int l=0,r=n-1,fi=0;
+        while(l<=r){
             int m=(l+r)/2;
             if(s[m]>k){
                 r=m-1;
             }else if(s[m]<k){
                 l=m+1;
             }else if(s[m]==k){
+                fi=1;
                 cout << m+1<<endl;
                 break;
             }
+        }
+        if(fi)
+            continue;
+        if(s[l]!=k){
+            cout << "not find"<<endl;
+        }else{
+            cout << s[l]<<endl;
         }
     }
 }
