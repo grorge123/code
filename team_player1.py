5a510d0497ad06be5fb42fc553f2253260d445a7
grorge123 <g97116@yahoo.com>
2018-07-16 21:31:34 +0800

    Update team_player1.py

--- team_player1.py
+++ team_player1.py
@@ -2,11 +2,7 @@ from AI.base import *
 from pygame.math import Vector2 as Vec
 
 import random
-def Abs(a)
-    if(a>0):
-        return a
-    else:
-        return a*-1
+
 class TeamAI( BaseAI ):
     def __init__( self , helper ):
         self.helper = helper
@@ -14,16 +10,14 @@ class TeamAI( BaseAI ):
 
     def decide( self ):
         helper = self.helper
-        allbull = helper.getAllPlayerBullet()
-        now = helper.getMyHeadPos()
-        if helper.getMyGrav() == None:
-            if helper.getDashPos() in allbull:
-                return AI_NothingToDo
-            else:
+        hPos = helper.getMyHeadPos()
+        wb_radius = helper.getWhiteballRadius()
+        if helper.getOtherBulletNumInRange(hPos, 3 * wb_radius) > 0:
+            return AI_MoveWayChange
+        if not helper.checkMeInGrav():
+            if helper.bodyOnRoute() or helper.headOnRoute():
                 return AI_MoveWayChange
-        else:
-            nodo = True
-            for i in allbull:
-                if not (Abs(now[0]-i[0])>2 and Abs(now[1]-i[1])>2):
-                    nodo = False
-            return AI_NothingToDo if nodo else AI_MoveWayChange
+        else :
+            if helper.checkMeCircling():
+                return AI_MoveWayChange
+        return AI_NothingToDo
