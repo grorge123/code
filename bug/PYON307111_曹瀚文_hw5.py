a0434832258dea620bde98fee28e09a0aa292926
grorge123 <g97116@yahoo.com>
2019-01-25 20:56:18 +0800

    Add files via upload

---
+++ bug/PYON307111_曹瀚文_hw5.py
@@ -0,0 +1,6 @@
+stu={"柯南","灰原","步美","美環","光彦","丸尾","野口"}
+math={"柯南","灰原","步美","美環","光彦"}
+eng={"柯南","灰原","丸尾","野口","步美"}
+print(math.difference(eng));
+print(eng.difference(math));
+print(eng.union(math));
