47339fcc17e6c80546b2e7b5d84f00f14ada4e12
grorge <g97116@yahoo.com>
2019-04-02 20:41:13 +0800

    delete project

--- project1/project1/MyForm.cpp
+++
@@ -1,9 +0,0 @@
-#include "MyForm.h"
-
-using namespace project1;
-int main(void) {
-	Application::EnableVisualStyles();
-	Application::SetCompatibleTextRenderingDefault(false);
-	Application::Run(gcnew MyForm());
-	return 0;
-}
