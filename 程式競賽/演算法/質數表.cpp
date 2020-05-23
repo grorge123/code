6b7f71b6506b870554ff9b996100b25cc8363b4f
grorge123 <g97116@yahoo.com>
2020-05-23 20:59:07 +0800

    can't understand the code, so i update new code

--- 程式競賽/演算法/質數表.cpp
+++ 程式競賽/演算法/質數表.cpp
@@ -1,34 +1,24 @@
 #include<bits/stdc++.h>
-#include<stdio.h>
 using namespace std;
-#define MA 2000000
-int s[2000005]={};
+int MAX = 0;
+bool s[100000000] = {};
+vector<int> ve;
+int prime(){
+    for(int i = 2 ; i <= MAX ; i++){
+        if(s[i])continue;
+        int tmp = 2;
+        ve.push_back(i);
+        while(tmp * i <= MAX){
+            s[tmp * i] = 1;
+            tmp++;
+        }
+    }
+}
 int main(){
-	for(int i=2;i<=MA;i++){
-		for(int q=2;q*i<=2000000;q++){
-			s[i*q]=1;
-		}
-	}
-	vector<int> ans;
-	int p=1,q=200000
-	;
-//	cin>> p>>q;
-	for(int i=p;i<=q;i++){
-		if(i<2)continue;
-		if(s[i]==0){
-			cout << i<<" ";
-			ans.push_back(i);
-		}
-	}
-	cout << endl;
-	for(vector<int>::iterator it=ans.begin();it!=ans.end();it++){
-		if(*it<p)continue;
-		vector<int>::iterator it2=it;
-		if(*it>q)break;
-		it2++;
-		if(it2==ans.end())break;
-		if(*it2-*it<3){
-			printf("%d %d\n",*it2,*it);
-		}
-	}
+    cin >> MAX;
+    prime();
+    for(auto i : ve){
+        cout << i << ' ' ;
+    }
+    return 0;
 }
