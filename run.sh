e03d547cc6499d91dc4fc0d27bc5b1cfa77231da
grorge123 <g97116@yahoo.com>
2020-10-25 19:57:41 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 test -e ./$1.out && rm ./$1.out
-g++ -std=c++11 -O2 -Wall -Wextra -o $1.out $1.cpp
+g++ -std=c++11 -O2 -Wall -Wextra -fsanitize=undefined -fsanitize=address -o $1.out $1.cpp
 test -e ./$1.out && ./$1.out
